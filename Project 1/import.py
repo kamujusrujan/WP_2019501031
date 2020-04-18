41b5aa5cff6b8c60bb991fe839c53ece3fab6131
kamujusrujan <kamujusrujan01@gmail.com>
2020-04-18 19:13:30 +0530

    'update'

--- Project 1/import.py
+++ Project 1/import.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 import csv
 
-engine = create_engine("postgresql://postgres:srujan@localhost:5432/test")
+engine = create_engine("postgres://birawtnmsapodw:678f377d616ec505fb180996a6ecab8f3a3aaa51e47ab7719590df97ce7872ae@ec2-18-233-32-61.compute-1.amazonaws.com:5432/dad3mt6v6b5qe4")
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
@@ -23,7 +23,7 @@ class Book(Base):
 data = open('books.csv','rt')
 Base.metadata.create_all(engine)
 data = list(csv.reader(data))
-for a in data[1:]:
+for a in tqdm(data[1:]):
 	session.add( Book(isbn = a[0] , title = a[1] ,author = a[2] ,  year = int(a[3]) ))
 session.commit()
 
