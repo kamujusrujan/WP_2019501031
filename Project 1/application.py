41eb2e334ce2bb863ed741b6fd4235c03c1f6d27
kamujusrujan <kamujusrujan01@gmail.com>
2020-04-24 22:58:33 +0530

    made changes

--- Project 1/application.py
+++ Project 1/application.py
@@ -72,16 +72,17 @@ def index():
 
 
 
-@app.route('/rating',methods = ['GET','POST'])
+@app.route('/book/<id>',methods = ['GET','POST'])
 @login_required
-def rating():
-	isbn = '62049879';
+def rating(id):
+	# isbn = '62049879'
+	isbn = id
 	if(request.method == 'GET'):
 		return render_template('rating.html',list = Ratings.query.filter_by( isbn = isbn).all() )
 	
 
 	desc,stars = request.form.get('description'), request.form.get('rating')
-	db.session.add( Ratings(isbn = '62049879',mail = session['name'],star = stars,description = desc))
+	db.session.add( Ratings(isbn = isbn ,mail = session['name'],star = stars,description = desc))
 	db.session.commit()
 	# userlist = Ratings.query.all()
 	return render_template('rating.html',list = Ratings.query.filter_by(isbn = isbn ).all() )
