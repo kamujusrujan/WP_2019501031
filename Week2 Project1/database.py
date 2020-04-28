73900a2b91bac72f35716d5007fa932026e79b9e
kamujusrujan <kamujusrujan01@gmail.com>
2020-04-28 22:41:31 +0530

    added week2 module

---
+++ Week2 Project1/database.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+class User(db.Model):
+	__tablename__ = 'registeredUsers'
+	mail = db.Column(db.String,primary_key = True)
+	name = db.Column(db.String)
+	password = db.Column(db.String)
+	created_data  = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+
+
+	
